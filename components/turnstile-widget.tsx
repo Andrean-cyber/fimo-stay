09906bc6f9540e216aa41b3964c8c00e92f033e1
Andrean Yogha Saputra <81624857+Andrean-cyber@users.noreply.github.com>
2026-08-13 23:53:13 +0700

    perbaikan

--- components/turnstile-widget.tsx
+++ components/turnstile-widget.tsx
@@ -4,10 +4,14 @@ import { Turnstile } from '@marsidev/react-turnstile'
 
 export function TurnstileWidget({ onVerify }: { onVerify: (token: string) => void }) {
   return (
-    <Turnstile
-      siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY!}
-      onSuccess={onVerify}
-      options={{ theme: 'light' }}
-    />
+    <div className="w-full flex justify-center">
+      <div className="scale-[0.70] sm:scale-100 origin-center">
+        <Turnstile
+          siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY!}
+          onSuccess={onVerify}
+          options={{ theme: 'light' }}
+        />
+      </div>
+    </div>
   )
 }
